0c16e6e7db3f0bcc28f66f938fc135246210621e
abetancourth <adrianaburbano@hotmail.es>
2023-08-29 14:19:30 -0500

    Curso cypress

---
+++ cypress/e2e/2-advanced-examples/PrimaPrueba.cy.js
@@ -0,0 +1,12 @@
+
+
+describe('Primera conjunti de pruebas', function()
+{
+
+    //Caso 1
+    it('Ingresar pagina principal Atenea', function(){
+
+        cy.visit("http://automationpractice.com/index.php")
+    })
+
+})
