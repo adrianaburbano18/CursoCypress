079f24c6b70a33dda74603d39aaa93e5a5fbff46
abetancourth <adrianaburbano@hotmail.es>
2023-08-30 22:54:16 -0500

    Curso cypress prueba

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'q969qf',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
